dd1c478b3b3b74f6ad670c303b949caa13f28596
Pallavi <Pallavi@lbg.com>
2024-09-03 14:05:26 +0000

    change of name

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -10,7 +10,7 @@ public class App
     {
         System.out.println(sayHello());
         System.out.println(sayHelloToSomeone("Alice"));
-        System.out.println(sayHelloToSomeone("Bob"));
+        System.out.println(sayHelloToSomeone("Pallavi"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
